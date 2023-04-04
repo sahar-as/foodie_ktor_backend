72677cf8c629d2c14ef6ff3a1c465fa7fa3ade34
sahar-as <sahar.asadian90@gmail.com>
2023-04-04 15:38:39 +0330

    updata commit, fix windows issue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+//buildscript {
+//    repositories {
+//        maven {
+//            url = uri("https://plugins.gradle.org/m2/")
+//        }
+//    }
+//    dependencies {
+//        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
+//    }
+//}
+//
+//apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
 rootProject.name = "ir.saharapps.foodie_ktor_backend"
